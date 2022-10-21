e4f96ba8729b54325ac6fd36d600770768102a34
Jeremiah Jacob <jaykon4me@gmail.com>
2022-10-20 19:40:53 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,14 @@
  * Return: no return
  */
 
-void more_numbers(void)
-{	
-	int i, j;
-	for (i = 1; i <= 10; i++)
+void print_more_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
-		}
-		_putchar('\n');
+		if (!(c == '2' || c == '4'))
+			_putchar(c);
 	}
+	_putchar('\n');
 }
